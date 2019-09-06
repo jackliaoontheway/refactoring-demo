f1f8d40b4d8d470d0ffe558cefd071a8b4a763e2
Jack <ldhontheway@live.com>
2019-09-06 22:44:24 +0800

    提炼函数 amountFor()

--- chapter1/application.js
+++ chapter1/application.js
@@ -7,24 +7,7 @@ function statement(invoice, plays) {
     const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unkonw type : ${play.type}`);
-        }
+        let thisAmount = amountFor(play, perf);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
@@ -41,4 +24,26 @@ function statement(invoice, plays) {
     result += `Amount owed is ${format(totalAmount / 100)}\n`
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+}
+
+function amountFor(play, perf) {
+    let thisAmount = 0;
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unkonw type : ${play.type}`);
+    }
+    return thisAmount;
 }
