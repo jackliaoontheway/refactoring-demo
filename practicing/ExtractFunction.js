a67401c7872d73651f832afae87f19850d6f7efc
Jack <ldhontheway@live.com>
2019-09-13 08:22:03 +0800

    提炼函数 demo

--- practicing/ExtractFunction.js
+++ practicing/ExtractFunction.js
@@ -1,22 +1,62 @@
 function printOwing(invoice) {
-    printBanner();
-    let outstanding = calculateOutstanding();
+
+    console.log("********************");
+    console.log("*** Customer Owes***");
+    console.log('********************');
+
+    let outstanding = 0;
+    // calculate outstanding
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
+    }
+
+    // record due date 
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
 
     //print details
     console.log(`name : ${invoice.customer}`);
     console.log(`amount : ${outstanding}`);
+    console.log(`due : ${invoice.dueDate.toLocaleDateString()}`);
 }
 
-/**
- * refactor
- */
+
+// Refactoring ======== >>
 
 function printOwing(invoice) {
-    printBanner();
-    let outstanding = calculateOutstanding();
+    // 无局部变量的 提炼函数
+    printBanner(); 
+    
+    // 有局部变量 并赋值 使用返回值赋值 
+    const outstanding = calculateOutstanding(invoice);
 
-    function printDetails(outstanding) {
-        console.log(`name : ${invoice.customer}`);
-        console.log(`amount : ${outstanding}`);
+    // 有局部变量的 提炼函数 将变量作为参数传递
+    recordDueDate(invoice);
+    // 有局部变量的 提炼函数 将变量作为参数传递
+    printDetails(invoice, outstanding);
+}
+
+function printBanner() {
+    console.log("********************");
+    console.log("*** Customer Owes***");
+    console.log('********************');
+}
+
+function calculateOutstanding(invoice) {
+    let result = 0;
+    for (const o of invoice.orders) {
+        result += o.amount;
     }
+    return result;
+}
+
+function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+}
+
+function printDetails(invoice, outstanding) {
+    console.log(`name : ${invoice.customer}`);
+    console.log(`amount : ${outstanding}`);
+    console.log(`due : ${invoice.dueDate.toLocaleDateString()}`);
 }
